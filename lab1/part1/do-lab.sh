995a5c1209dfe684c15dcbe14ea06b648ee3610c
tylerpaik <tyler.paik@gmail.com>
2024-10-07 12:39:29 -0400

    do-lab

--- lab1/part1/do-lab.sh
+++ lab1/part1/do-lab.sh
@@ -3,3 +3,13 @@
 # INCLUDE ALL COMMANDS NEEDED TO PERFORM THE LAB
 # This file will get called from capture_submission.sh
 
+source ./provided/make_aliases.sh
+sudo containerlab deploy -t 4node-part1.clab.yml
+./provided/change_mac_addrs.sh
+
+for i in {1..4}; do
+    docker cp ./provided/onepkt.py clab-lab1-part1-host$i:/lab-folder/onepkt.py
+done
+
+host1 /lab-folder/onepkt.py host1 host2 test-pkt1
+host2 /lab-folder/onepkt.py host2 host3 test-pkt2
