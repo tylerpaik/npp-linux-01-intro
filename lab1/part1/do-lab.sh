7caa751ef79762098d684ce6bf43dc44f85ff8cd
tylerpaik <tyler.paik@gmail.com>
2024-10-16 19:44:22 -0400

    this worked

--- lab1/part1/do-lab.sh
+++ lab1/part1/do-lab.sh
@@ -3,18 +3,47 @@
 # INCLUDE ALL COMMANDS NEEDED TO PERFORM THE LAB
 # This file will get called from capture_submission.sh
 
-docker exec clab-lab1-part1-host1 ip addr add 1.1.1.1/24 dev eth1
-docker exec clab-lab1-part1-host1 ip link set dev eth1 up
-docker exec clab-lab1-part1-host1 ip route add default via 1.1.1.254
+install_packages() {
+    local container=$1
+    docker exec -it $container apt-get update
+    docker exec -it $container apt-get install -y bridge-utils python3-pip tshark
+}
 
-docker exec clab-lab1-part1-host2 ip addr add 1.1.1.2/24 dev eth1
-docker exec clab-lab1-part1-host2 ip link set dev eth1 up
-docker exec clab-lab1-part1-host2 ip route add default via 1.1.1.254
+install_packages clab-lab1-part1-switch
 
-docker exec clab-lab1-part1-host3 ip addr add 1.1.1.3/24 dev eth1
-docker exec clab-lab1-part1-host3 ip link set dev eth1 up
-docker exec clab-lab1-part1-host3 ip route add default via 1.1.1.254
+for host in host1 host2 host3 host4
+do
+    install_packages clab-lab1-part1-$host
+done
 
-docker exec clab-lab1-part1-host4 ip addr add 1.1.1.4/24 dev eth1
-docker exec clab-lab1-part1-host4 ip link set dev eth1 up
-docker exec clab-lab1-part1-host4 ip route add default via 1.1.1.254
+docker exec -it clab-lab1-part1-switch brctl addbr br0
+
+docker exec -it clab-lab1-part1-switch brctl addif br0 eth1
+docker exec -it clab-lab1-part1-switch brctl addif br0 eth2
+docker exec -it clab-lab1-part1-switch brctl addif br0 eth3
+docker exec -it clab-lab1-part1-switch brctl addif br0 eth4
+
+docker exec -it clab-lab1-part1-switch ip link set br0 up
+
+bring_up_interface() {
+    local host=$1
+    docker exec -it clab-lab1-part1-$host ip link set eth1 up
+}
+
+for host in host1 host2 host3 host4
+do
+    bring_up_interface $host
+done
+
+docker exec -it clab-lab1-part1-switch brctl show
+
+for host in host1 host2 host3 host4
+do
+    docker exec -it clab-lab1-part1-$host ip addr show eth1
+done
+
+for host in host1 host2 host3 host4
+do
+    docker exec -it clab-lab1-part1-$host python3 -c "import scapy; print('Scapy is installed')"
+done
+
